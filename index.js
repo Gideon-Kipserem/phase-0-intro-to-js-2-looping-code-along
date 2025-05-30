1d135d9ddddb8b47c8069fb34c060f1431cdfc07
Gideon <gideonkimaiyo254@gmail.com>
2025-05-30 09:05:16 +0300

    uses for and while to loop

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+
+}
+
+  return messages;
+}
+const names = ["Charlie", "Samip", "Ali"];
+
+const thankYouMessages = writeCards(names, "birthday");
+
+console.log(thankYouMessages);
+
+function countDown() {
+    let countup = 10;
+while (countup >= 0) {
+  console.log(countup--);
+}
+}
